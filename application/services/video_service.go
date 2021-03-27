cd1a246a3c9aaa8e9217184211bd74e6d9139b9a
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-03-27 16:03:48 -0300

    feat: Implementing download and fragment methods

--- application/services/video_service.go
+++ application/services/video_service.go
@@ -13,7 +13,7 @@ import (
 
 type VideoService struct {
 	Video           *domain.Video
-	VideoRepository *repositories.VideoRepository
+	VideoRepository repositories.VideoRepository
 }
 
 func NewVideoService() VideoService {
--- application/services/video_service_test.go
+++ application/services/video_service_test.go
@@ -1,1 +1,49 @@
-package services
+package services_test
+
+import (
+	"github.com/joho/godotenv"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/valdirmendesdev/encoder-service/application/repositories"
+	"github.com/valdirmendesdev/encoder-service/application/services"
+	"github.com/valdirmendesdev/encoder-service/domain"
+	"github.com/valdirmendesdev/encoder-service/framework/database"
+	"log"
+	"testing"
+	"time"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func prepare() (*domain.Video, repositories.VideoRepositoryDb) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "convite.mp4"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	return video, repo
+}
+
+func TestVideoServiceDownload(t *testing.T) {
+
+	video, repo := prepare()
+
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("codeeducationtest")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+}
